37d7ecd492be7bf384f3fc0bf3307b43ccade887
linchloe <cat1206tw@gmail.com>
2022-07-30 23:20:24 -0500

    "Update for null case"

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return null;
+        }
         IntList temp = A;
           while (A.rest != null) {
               A = A.rest;
@@ -95,6 +98,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null) {
+            return null;
+        }
         IntList nL = new IntList(A.first, null);
         A = A.rest;
         IntList temp = nL;
